6c9b21322ab7f21f563fa3196f510303ce6fc4bd
ayush2297 <ayush.saraf47@gmail.com>
2019-11-14 16:19:43 +0530

    rolling the die and moving player till 100th position is reached. resetting the position to 0 if it goes below 0

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -4,13 +4,14 @@ echo "************welcome to the game of snake and ladder**********"
 
 #Constants
 declare INITIAL_PLAYER_POSITION=0
+declare WINNING_POSITION=100
 declare NO_PLAY=0
 declare LADDER=1
 declare SNAKE=2
 
 #variables
 declare dieResult
-declare playerPosition
+declare playerPosition=0
 
 function checkOptions(){
 	chosenOption=$((RANDOM%3))
@@ -20,12 +21,19 @@ function checkOptions(){
 		$LADDER)
 			playerPosition=$(( $playerPosition + $dieResult ));;
 		$SNAKE)
-			playerPosition=$(( $playerPosition + $dieResult ));;
+			playerPosition=$(( $playerPosition - $dieResult ));;
 		*)
 			;;
 	esac
 }
 
-dieResult=$(( $((RANDOM%6)) + 1 ))
-playerPosition=$INITIAL_PLAYER_POSITION
-checkOptions
+
+while [ $playerPosition -le $WINNING_POSITION ]
+do
+	if [ $playerPosition -le $INITIAL_PLAYER_POSITION ]
+	then
+		playerPosition=$INITIAL_PLAYER_POSITION
+	fi
+	dieResult=$(( $((RANDOM%6)) + 1 ))
+	checkOptions
+done
