54cdab616a409a8384959f7e5b4c562e664608b3
ayush2297 <ayush.saraf47@gmail.com>
2019-11-14 15:48:43 +0530

    added die rolling functionality using random to generate a random number between 1 and 6

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -2,6 +2,11 @@
 
 echo "************welcome to the game of snake and ladder**********"
 
-
 #Constants
 declare INITIAL_PLAYER_POSITION=0
+
+#variables
+declare dieResult
+
+
+dieResult=$(( $((RANDOM%6)) + 1 ))
