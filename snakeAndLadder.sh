c9e933f5664125f8fd2cd9aa0cc07578dd8e71e0
ayush2297 <ayush.saraf47@gmail.com>
2019-11-14 16:08:04 +0530

    added game functionalities wherein player stays, moves forward or goes back number of positions specified by roll die. the decision is taken using random

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -4,9 +4,28 @@ echo "************welcome to the game of snake and ladder**********"
 
 #Constants
 declare INITIAL_PLAYER_POSITION=0
+declare NO_PLAY=0
+declare LADDER=1
+declare SNAKE=2
 
 #variables
 declare dieResult
+declare playerPosition
 
+function checkOptions(){
+	chosenOption=$((RANDOM%3))
+	case $chosenOption in 
+		$NO_PLAY)
+			playerPosition=$playerPosition;;
+		$LADDER)
+			playerPosition=$(( $playerPosition + $dieResult ));;
+		$SNAKE)
+			playerPosition=$(( $playerPosition + $dieResult ));;
+		*)
+			;;
+	esac
+}
 
 dieResult=$(( $((RANDOM%6)) + 1 ))
+playerPosition=$INITIAL_PLAYER_POSITION
+checkOptions
